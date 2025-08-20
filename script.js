2e2ff309e463444aa86a78510a4a4c5e617a6eac
Irman Wibawa <irman.w@hotmail.com>
2025-08-20 16:08:56 +1000

    added project & contact sections

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ toggleBtn.addEventListener("click", () => {
   document.body.classList.toggle("dark-mode");
 });
 
-// Fun Fact Button
+// Fun Fact Button - update facts to proper ones later
 const factBtn = document.getElementById("fun-fact-btn");
 const factDisplay = document.getElementById("fun-fact");
 
@@ -18,4 +18,11 @@ const facts = [
 factBtn.addEventListener("click", () => {
     const randomFact = facts[Math.floor(Math.random() * facts.length)];
     factDisplay.textContent = randomFact;
+});
+
+//Form Validaiton
+const form = document.getElementById("contact-form");
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    alert("Thanks for reaching out! I'll get back to you soon.")
 });

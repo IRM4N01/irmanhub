98485a33bc706683919d688d466103c796f2eccb
Irman Wibawa <irman.w@hotmail.com>
2025-08-20 12:28:59 +1000

    added intro & abouct sections w/ operational JS and styles

--- script.js
+++ script.js
@@ -3,3 +3,19 @@ const toggleBtn = document.getElementById("toggle-dark");
 toggleBtn.addEventListener("click", () => {
   document.body.classList.toggle("dark-mode");
 });
+
+// Fun Fact Button
+const factBtn = document.getElementById("fun-fact-btn");
+const factDisplay = document.getElementById("fun-fact");
+
+const facts = [
+    "fun fun fun fun",
+    "fact fact fact fact",
+    "put proper facts in later",
+    "software engineer"
+];
+
+factBtn.addEventListener("click", () => {
+    const randomFact = facts[Math.floor(Math.random() * facts.length)];
+    factDisplay.textContent = randomFact;
+});
